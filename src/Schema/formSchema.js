300e02482ae21f5204ee716239142b939c1534c7
Rick <rmansfield@realpeoplesratings.com>
2021-07-26 17:13:06 -0500

    add some formSchema.js shape

--- src/Schema/formSchema.js
+++ src/Schema/formSchema.js
@@ -2,8 +2,18 @@ import * as yup from 'yup'
 // import { boolean } from 'yup/lib/locale'
 
 export default yup.object().shape({
-    username: yup
+    nickname: yup
         .string()
-        .required("Username is requires")
+        .required("Plant name is required")
         .min(2, "name must be at least 2 characters"),
+        species: yup
+        .string()
+        .required("Species is required")
+        .min(2, "name must be at least 2 characters"),
+        h2ofrequency: yup
+        .string()
+        .required("Water Frequency is required")
+        .min(2, "name must be at least 2 characters"),
+        image: yup.boolean(),
+
     })
