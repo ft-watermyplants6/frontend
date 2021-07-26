91a3d894b8f1b846282c101c14bb057c1dc978f0
Cara Locke <cara.m.locke@gmail.com>
2021-07-26 13:20:30 -0400

    Fixed a bug causing rendering error.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import LoginForm from './components/LoginForm';
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <LoginForm/>
-=======
       <h1>Water My Plants</h1>
->>>>>>> c4a448b712920b979f7883880ff6319abfbcad44
+      <LoginForm/>
     </div>
   );
 }
