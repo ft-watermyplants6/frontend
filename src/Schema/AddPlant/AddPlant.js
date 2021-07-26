21325350a1011f24769dba90e992aca81b8665a9
Rick <rmansfield@realpeoplesratings.com>
2021-07-26 14:09:56 -0500

    Add state slices and axios GET/POST placeholders

--- src/Schema/AddPlant/AddPlant.js
+++ src/Schema/AddPlant/AddPlant.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 // import * as yup from "yup";
 // import schema from "./validations/formSchema";
-// import axios from "axios";
+import axios from "axios";
+
 const initialFormValues = {
     plantname: "",
     species: "",
@@ -21,7 +22,40 @@ const initialDisabled = true;
 
 
 export default function AddPlant(){
-    const [plants, setPlants] = useState(plantList)
+    const [plants, setPlants] = useState(plantList);
+    const [formValues, setFormValues] = useState(initialFormValues);
+    const [formErrors, setFormErrors] = useState(initialFormErrors);
+    const [disabled, setDisabled] = useState(initialDisabled);
+    
+    const plantSubmitted = (newplant) => {
+        return setPlants(formValues)
+    } 
+
+     //   axios
+  //     .get("https://reqres.in/api/orders")
+  //     .then((res) => {
+  //       setPizzas(res.data.data);
+  //       console.log(`HERE IS setPizzas`, setPizzas);
+  //     })
+  //     .catch((err) => {
+  //       debugger;
+  //       console.log(err);
+  //     });
+  // };
+
+  const postNewPlant = (newPlant) => {
+    axios
+      .post("???????", newPlant)
+      .then((res) => {
+        setPlants([...plants, res.data]);
+        setFormValues(initialFormValues);
+        console.log(`HERE is postNewPlant`, postNewPlant);
+      })
+      .catch((err) => {
+        debugger;
+        console.log(err);
+      })
+  };
 
     
     return(
