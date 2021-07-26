ebc82ec76abb1b82c2cbb28a1f22ad148dee8a55
Rick <rmansfield@realpeoplesratings.com>
2021-07-26 12:28:33 -0500

    App.js Nav section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import './App.css';
 function App() {
   return (
     <div>
+      <AddPlant>
+
+      </AddPlant>
+      <nav className="home-login-subscribe">
+                    <a href="index.html">HOME</a>
+                    <a href="index.html">LogIn/Out</a>
+                    <a href="index.html">Subscribe</a>
+                    <Link to={'/add'}>
+                        <button id="add-plant">Add a Plant</button>
+                    </Link>
+                    <Link to={'/edit'}>
+                        <button id="edit-plant">Edit a Plan</button>
+                    </Link>
+                </nav>
 
     </div>
   );
