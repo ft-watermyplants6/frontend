dfb697197ed37fc33936be6f1f0773cc3228597e
Rick <rmansfield@realpeoplesratings.com>
2021-07-28 14:08:08 -0500

    start signup form styling

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios';
+import styled from 'styled-components';
 
 class SignupForm extends React.Component {
 
@@ -32,43 +33,70 @@ class SignupForm extends React.Component {
       .catch(err => {
         console.log(err);
       })
-    }
+  }
 
-    render() {
-        return(
-            <div>
-              <form>
-                  <label>Username: 
-                    <input 
-                     type='text'
-                     name='username'
-                     value={this.state.credentials.username}
-                     onChange={this.handleChange}
-                    />
-                  </label>
-                  <label>Phone Number: 
-                    <input 
-                      type='tel'
-                      name='phoneNumber'
-                      pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
-                      value={this.state.credentials.phoneNumber}
-                      onChange={this.handleChange}
-                    />
-                  </label>
-                  <label>Password: 
-                    <input 
-                      type='password'
-                      name='password'
-                      value={this.state.credentials.password}
-                      onChange={this.handleChange}
-                    />
-                  </label>
-                    <button>Sign Up</button>
-              </form>
-            </div>
-            
-        )
-    }
+  render() {
+    return (
+      <Container>
+      <div>
+        <form>
+          <label>Username:
+            <input
+              type='text'
+              name='username'
+              value={this.state.credentials.username}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>Phone Number:
+            <input
+              type='tel'
+              name='phoneNumber'
+              pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}'
+              value={this.state.credentials.phoneNumber}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>Password:
+            <input
+              type='password'
+              name='password'
+              value={this.state.credentials.password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button>Sign Up</button>
+        </form>
+      </div>
+      </Container>
+
+    )
+  }
 }
 
 export default SignupForm
+
+// const Image = styled.img`
+// border: 1px solid black;
+// margin: 3% ;
+// height: 60vh; `
+
+
+const Container = styled.div`
+form{
+  margin: 1%;
+  display: flex;
+  flex-direction: column;
+	justify-content: space-space-between;
+	align-items: center; 
+
+}
+label{
+  margin: 1%;
+
+
+}
+button{
+
+}
+`;
