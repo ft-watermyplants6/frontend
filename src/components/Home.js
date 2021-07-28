fdbd16672bbc4b68597d3d8c282aa89a972fdfb3
Rick <rmansfield@realpeoplesratings.com>
2021-07-28 11:11:42 -0500

    Add Home Page Content

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,15 @@ export default function Home() {
                     src="https://images.unsplash.com/photo-1599277100479-3252d492a19a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0ZXJpbmclMjBjYW58ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
                     alt="plant"
                 />
+                <div>
+                    <h2>What we do</h2>
+                <p>
+                    Love plants but hate the pesky watering schedule? No worries "Water My Plants" is here to help. 
+                </p>
+                <p>Add your plant's name, species, image and waterting schedule</p>
+                <p> and we'll help you remember to water on time</p>
+                </div>
+
             </div>
         </Container>
     );
