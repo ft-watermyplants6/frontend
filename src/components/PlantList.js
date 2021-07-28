fff98d9a434691fde87ca8b3cadd6687fcd07e18
Rick <rmansfield@realpeoplesratings.com>
2021-07-28 09:46:45 -0500

    Added PlantList.js Styling

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,36 +1,53 @@
 import React, { Component } from 'react'
 import { getPlants } from '../services/PlantsService'
+import styled from 'styled-components'
 
 export default class PlantList extends Component {
-    state={
+    state = {
 
         plants: []
     }
 
     componentDidMount() {
         getPlants()
-             .then(res => {
-                 console.log(res)
-                 this.setState({
-                     plants: res.data.results
-                 })
-             })
-             .catch(err => {
-                 console.log(err)
-             })
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    plants: res.data.results
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }
 
     render() {
         return (
-            <main className='plant-list'>
-                {this.state.plants.map((plant) => (
-                    <div className='plant-card' key={plant.name}>
-                        <div className='plant-details'>
-                            <h2>{plant.name}</h2>
+            <Plantlist>
+                <main className='plant-list'>
+                    {this.state.plants.map((plant) => (
+                        <div className='plant-card' key={plant.name}>
+                            <div className='plant-details'>
+                                <h2>{plant.name}</h2>
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </main>
+                    ))}
+                </main>
+            </Plantlist>
         )
     }
 }
+
+
+const Plantlist = styled.div`
+.plant-card {
+  background-color: #fff;
+  border: 0;
+  box-shadow: 0 -1px 0 #e0e0e0, 0 0 2px rgba(0, 0, 0, 0.12),
+    0 2px 4px rgba(0, 0, 0, 0.24);
+  padding: 1rem;
+  cursor: pointer;
+  position: relative;
+  margin: 1rem auto;
+}
+`;
