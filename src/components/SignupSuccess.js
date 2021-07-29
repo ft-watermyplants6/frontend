6a441c0b3935340eb89ad9a56cf88ab8f8e584f9
Rick <rmansfield@realpeoplesratings.com>
2021-07-29 09:27:45 -0500

    add SignUpSuccess.js

---
+++ src/components/SignupSuccess.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function SignUPSuccess() {
+    return (
+        <section>
+            <div id='signup-success-div'>
+                WELCOME!
+            </div>
+            <p>select a button above to get started</p>
+        </section>
+    )
+}
